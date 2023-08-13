8f2b2bb6b8e9c63d0aff549f0e793a632b2a5703
Margulis162 <markmargulis@proton.me>
2023-08-13 15:59:15 -0400

    lists added to loop through the pics and also the loop itself

--- main.js
+++ main.js
@@ -5,14 +5,21 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const Names = "pic1.jpg  pic2.jpg  pic3.jpg  pic4.jpg  pic5.jpg";
+const imgNames = Names.split('  ');
+
+const imgAlts =['close up of an eye', 'something wavy', 'flowers', 'ancient egyptian wall art', 'butterfly'];
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(let i = 0; i < imgNames.length; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${imgNames[i]}`);
+    newImage.setAttribute('alt', imgAlts[i]);
+    thumbBar.appendChild(newImage);
+    console.log(imgNames.length);
+    
+}
 
 /* Wiring up the Darken/Lighten button */
